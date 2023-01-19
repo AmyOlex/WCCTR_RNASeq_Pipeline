b3b0d19adb07527c078784de6a01f5ed509720d2
Amy Olex <alolex@vcu.edu>
2023-01-19 16:38:26 -0500

    updated to include command line options.

--- SingleCell/scTyper_script.R
+++ SingleCell/scTyper_script.R
@@ -4,29 +4,83 @@
 
 library(Seurat)
 library(scTyper)
+library(optparse)
 
-setwd("/vcu_gpfs2/home/harrell_lab/collaboration_scRNAseq/visvader_results_2021/scTyper_analysis/")
+option_list = list(
+  make_option(c("-r", "--runid"), type="character", default=NULL, 
+              help="Required. A unique name for this analysis.", metavar="character"),
+  make_option(c("-o", "--outdir"), type="character", 
+              help="output directory for results report (must already exist). Default is current directory.",
+              default = "./", metavar="character"),
+  make_option(c("-f", "--seuratfile"), type="character", 
+              help="Seurat object saved as an RData file in a variable named seurat.merged.", metavar="character"),
+  
+  make_option(c("-s", "--sigfile"), type="character", 
+              help="A TSV file with the gene signatures to use for cell type classification.", metavar="character"),
+  make_option(c("-p", "--phenofile"), type="character", 
+              help="A CSV file with the sample annotations must be provided.", metavar="character"),
+  
+  make_option(c("-t", "--typelevel"), type="character", 
+              help="Optional. The level of cell typing. Options are cell (default) or cluster.", 
+              default = "cell", metavar="character"),
+  
+  
+); 
+
+opt_parser = OptionParser(option_list=option_list);
+opt = parse_args(opt_parser);
+
+# test for NULL required arguments
+if (is.null(opt$runid)){
+  print_help(opt_parser)
+  stop("A unique analysis name must be provided.", call.=FALSE)
+}
+
+if (is.null(opt$seuratfile)){
+  print_help(opt_parser)
+  stop("A Seurat object saved as a RData file must be provided.", call.=FALSE)
+}
+
+if (is.null(opt$sigfile)){
+  print_help(opt_parser)
+  stop("A TSV formatted gene siguature file must be provided.", call.=FALSE)
+}
+
+if (is.null(opt$phenofile)){
+  print_help(opt_parser)
+  stop("A CSV formatted sample phenotype file must be provided.", call.=FALSE)
+}
+
+
+runID <- opt$runid  ## "100422_Visvader-plussss-PDX_MASTER_30_SimpleMerge_GRCh38_Seurat_simpleMerge_LogNormalize.scTyper.Average.output.121922"
+wd <- opt$outdir  ## "/vcu_gpfs2/home/harrell_lab/collaboration_scRNAseq/visvader_results_2021/scTyper_analysis/"
+seuratfile <- opt$seuratfile ## "100422_Visvader-plussss-PDX_MASTER_30_SimpleMerge_GRCh38_Seurat_simpleMerge_LogNormalize_Annotated.RData"
+sigfile <- opt$sigfile  ## "scTyperSignatures_12.19.22.tsv"
+phenofile <- opt$phenofile  ## "SeuratSimpleMerge_Visvader-plus-PDX_noUNC_GRCh38_100422_scTyper.MetaData.csv"
+typelevel <- opt$typelevel  ## "cell"
+
+setwd(wd)
 
 #load in data set
-load("100422_Visvader-plussss-PDX_MASTER_30_SimpleMerge_GRCh38_Seurat_simpleMerge_LogNormalize_Annotated.RData")
+load(seuratfile)
 
 #add a sample.name annotation
 seurat.merged$sample.name <- seurat.merged$orig.ident
 
 #load in the list of signatures to use
-sigs <- read.delim("scTyperSignatures_12.19.22.tsv", header=TRUE)
+sigs <- read.delim(sigfile, header=TRUE)
 
 #run scTyper
 celltyped.seurat=scTyper(seurat.object=seurat.merged,
                          marker=sigs$Identifier,
-                         wd = getwd(),
-                         output.name = "100422_Visvader-plussss-PDX_MASTER_30_SimpleMerge_GRCh38_Seurat_simpleMerge_LogNormalize.scTyper.Average.output.121922",
-                         pheno.fn = "SeuratSimpleMerge_Visvader-plus-PDX_noUNC_GRCh38_100422_scTyper.MetaData.csv",
+                         wd = wd,
+                         output.name = runID,
+                         pheno.fn = phenofile,
                          qc = FALSE,
                          run.cellranger=FALSE,
                          norm.seurat=FALSE,
                          cell.typing.method="Average",
-                         level="cell",
+                         level=typelevel,
                          run.inferCNV=FALSE,
                          proj.name = "scTyper",
                          gene.ref.gtf="/vcu_gpfs2/home/mccbnfolab/ref_genomes/CellRanger/refdata-cellranger-GRCh38-3.0.0/genes/genes.gtf",
@@ -38,9 +92,9 @@ celltyped.seurat=scTyper(seurat.object=seurat.merged,
                          mc.cores = 30)
 
 #save cell.type annotation to CSV file
-write.csv(data.frame(barcode = names(celltyped.seurat@active.ident), scTyperAvg.cellType = celltyped.seurat@meta.data$cell.type), file="100422_Visvader-plussss-PDX_MASTER_30_SimpleMerge_GRCh38_Seurat_simpleMerge_LogNormalize_scTyperAverage.cellType_12.19.22.csv", quote = FALSE, row.names = FALSE)
+write.csv(data.frame(barcode = names(celltyped.seurat@active.ident), scTyperAvg.cellType = celltyped.seurat@meta.data$cell.type), file=paste0(runID,".csv", quote = FALSE, row.names = FALSE))
 
 #save seurat object
-saveRDS(celltyped.seurat, file = "100422_Visvader-plussss-PDX_MASTER_30_SimpleMerge_GRCh38_Seurat_simpleMerge_LogNormalize_scTyperTyped_12.19.22.rds")
+saveRDS(celltyped.seurat, file = paste0(runID, "_scTyperTyped.rds"))
 
 
